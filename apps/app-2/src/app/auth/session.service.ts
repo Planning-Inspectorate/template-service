91db8c6b738bc1395a025f49bf2a8896cdb22fd7
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-09-29 12:16:06 +0100

    chore: session.service.ts jsdoc -> ts

--- apps/app-2/src/app/auth/session.service.ts
+++ apps/app-2/src/app/auth/session.service.ts
@@ -1,32 +1,34 @@
-/** @typedef {import('express-session').Session & AuthState} SessionWithAuth */
-/** @typedef {import('@azure/msal-node').AccountInfo} AccountInfo */
-/** @typedef {import('@azure/msal-node').AuthenticationResult} AuthenticationResult */
+import type { Session } from 'express-session';
+import type { AccountInfo, AuthenticationResult } from '@azure/msal-node';
 
-/**
- * @typedef {object} AuthenticationData
- * @property {string} nonce
- * @property {string} postSigninRedirectUri
- */
+export interface AuthenticationData {
+	nonce: string;
+	postSigninRedirectUri: string;
+}
 
-/**
- * @typedef {object} AuthState
- * @property {AccountInfo & {accessToken: string, expiresOnTimestamp?: number}} [account]
- * @property {AuthenticationData=} authenticationData
- */
+export interface AuthState {
+	account?: AccountInfo & {
+		accessToken: string;
+		expiresOnTimestamp?: number;
+	};
+	authenticationData?: AuthenticationData;
+}
+
+export type SessionWithAuth = Session & AuthState;
 
 /**
  * @param {SessionWithAuth} session
  * @returns {void}
  */
-export const destroyAuthenticationData = (session) => {
+export const destroyAuthenticationData = (session: SessionWithAuth): void => {
 	delete session?.authenticationData;
 };
 
 /**
- * @param {SessionWithAuth} session
- * @returns {AuthenticationData}
+ * @param session
+ * @returns
  */
-export const getAuthenticationData = (session) => {
+export const getAuthenticationData = (session: SessionWithAuth): AuthenticationData => {
 	if (session?.authenticationData) {
 		return session?.authenticationData;
 	}
@@ -38,7 +40,7 @@ export const getAuthenticationData = (session) => {
  * @param {AuthenticationData} data
  * @returns {void}
  */
-export const setAuthenticationData = (session, data) => {
+export const setAuthenticationData = (session: SessionWithAuth, data: AuthenticationData): void => {
 	session.authenticationData = data;
 };
 
@@ -46,16 +48,16 @@ export const setAuthenticationData = (session, data) => {
  * @param {SessionWithAuth} session
  * @returns {void}
  */
-export const destroyAccount = (session) => {
+export const destroyAccount = (session: SessionWithAuth): void => {
 	delete session?.account;
 };
 
 /**
- * @param {SessionWithAuth} session
- * @param {AuthenticationResult} authenticationResult
- * @returns {void}
+ * @param session
+ * @param authenticationResult
+ * @returns
  */
-export const setAccount = (session, authenticationResult) => {
+export const setAccount = (session: SessionWithAuth, authenticationResult: AuthenticationResult): void => {
 	const { account, accessToken, idToken, expiresOn } = authenticationResult;
 
 	if (!account) {
@@ -70,9 +72,9 @@ export const setAccount = (session, authenticationResult) => {
 };
 
 /**
- * @param {SessionWithAuth} session
- * @returns {AccountInfo=}
+ * @param session
+ * @returns
  */
-export const getAccount = (session) => {
+export const getAccount = (session: SessionWithAuth): AccountInfo | undefined => {
 	return session?.account;
 };

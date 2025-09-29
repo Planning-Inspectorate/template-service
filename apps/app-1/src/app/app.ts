2fa4dd99260e157290575dd769820cfaf4e994cf
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-09-29 12:16:34 +0100

    chore: app.ts jsdoc -> ts

--- apps/app-1/src/app/app.ts
+++ apps/app-1/src/app/app.ts
@@ -2,12 +2,13 @@ import { buildRouter } from './router.ts';
 import { configureNunjucks } from './nunjucks.ts';
 import { addLocalsConfiguration } from '#util/config-middleware.ts';
 import { createBaseApp } from '@pins/service-name-lib/app/app.ts';
+import type { Express } from 'express';
+import type { App1Service } from '#service';
 
 /**
- * @param {import('#service').App1Service} service
- * @returns {Express}
+ * @param service
  */
-export function createApp(service) {
+export function createApp(service: App1Service): Express {
 	const router = buildRouter(service);
 	// create an express app, and configure it for our usage
 	return createBaseApp({ service, configureNunjucks, router, middlewares: [addLocalsConfiguration()] });

5713c093c3ab995bd190a48a2d6377ce5a25c88d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-10-02 11:25:18 +0100

    chore: add AsyncRequestHandler type

--- packages/lib/util/async-handler.ts
+++ packages/lib/util/async-handler.ts
@@ -1,6 +1,23 @@
+import type core from 'express-serve-static-core';
+import type { RequestHandler } from 'express';
+
+// an async handler type, since RequestHandler is sync and does not return a Promise
+// adapted from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/8e274af6ed512811d15426cca3b946cd9227a255/types/express-serve-static-core/index.d.ts#L52-L65
+export type AsyncRequestHandler<
+	P = core.ParamsDictionary,
+	ResBody = any,
+	ReqBody = any,
+	ReqQuery = core.Query,
+	LocalsObj extends Record<string, any> = Record<string, any>
+> = (
+	req: core.Request<P, ResBody, ReqBody, ReqQuery, LocalsObj>,
+	res: core.Response<ResBody, LocalsObj>
+) => Promise<void>;
+
 export function asyncHandler<A, B, C, D, E extends Record<string, any>>(
-	requestHandler: import('express').RequestHandler<A, B, C, D, E>
-): import('express').RequestHandler<A, B, C, D, E> {
+	// supports async or sync handlers
+	requestHandler: RequestHandler<A, B, C, D, E> | AsyncRequestHandler<A, B, C, D, E>
+): RequestHandler<A, B, C, D, E> {
 	return (request, response, next) => {
 		try {
 			const p = requestHandler(request, response, next);

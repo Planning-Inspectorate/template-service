ea78f68b9ed1a2a6ed918cac8f1b8723e39bf90c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-17 12:12:53 +0000

    feat(manage): add some questions with conditional logic

--- apps/manage/src/app/views/cases/journey.ts
+++ apps/manage/src/app/views/cases/journey.ts
@@ -1,4 +1,4 @@
-import { Journey, type JourneyResponse, Section } from '@planning-inspectorate/dynamic-forms';
+import { Journey, type JourneyResponse, Section, whenQuestionHasAnswer } from '@planning-inspectorate/dynamic-forms';
 import type { Request } from 'express';
 
 export const JOURNEY_ID = 'create-a-case';
@@ -6,7 +6,14 @@ export const JOURNEY_ID = 'create-a-case';
 export function createJourney(req: Request, response: JourneyResponse, questions: Record<string, any>) {
 	return new Journey({
 		journeyId: JOURNEY_ID,
-		sections: [new Section('Questions', 'questions').addQuestion(questions.reference)],
+		sections: [
+			new Section('Questions', 'questions')
+				.addQuestion(questions.reference)
+				.addQuestion(questions.howManyApplicants)
+				.addQuestion(questions.submissionDate)
+				.withCondition(whenQuestionHasAnswer(questions.howManyApplicants, '5'))
+				.addQuestion(questions.description)
+		],
 		taskListUrl: 'check-your-answers',
 		journeyTemplate: 'views/layouts/layout-journey.njk',
 		taskListTemplate: 'views/layouts/layout-check-your-answers.njk',

5e3d76bbf993e6e3e42663fe2cda40a098660993
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-17 12:40:49 +0000

    fix(manage): ensure async controller are wrapped with asyncHandler

--- apps/manage/src/app/views/cases/index.ts
+++ apps/manage/src/app/views/cases/index.ts
@@ -14,6 +14,7 @@ import { createJourney, JOURNEY_ID } from './journey.ts';
 import { questions } from './questions.ts';
 import { buildSaveController } from './save.ts';
 import { createRoutes as createDetailsRoutes } from './view/index.ts';
+import { asyncHandler } from '@pins/service-name-lib/util/async-handler.ts';
 
 export function createRoutes(service: ManageService): IRouter {
 	const router = createRouter({ mergeParams: true });
@@ -21,21 +22,16 @@ export function createRoutes(service: ManageService): IRouter {
 	// read answers from the session
 	const getJourneyResponse = buildGetJourneyResponseFromSession(JOURNEY_ID);
 	const getJourney = buildGetJourney((req, journeyResponse) => createJourney(req, journeyResponse, questions));
+	const saveToSession = asyncHandler(buildSave(saveDataToSession));
+	const saveToDatabase = asyncHandler(buildSaveController(service));
 
 	router.use('/view/:id', createDetailsRoutes(service));
 
 	router.get('/:section/:question', getJourneyResponse, getJourney, question);
-	router.post(
-		'/:section/:question',
-		getJourneyResponse,
-		getJourney,
-		validate,
-		validationErrorHandler,
-		buildSave(saveDataToSession)
-	);
+	router.post('/:section/:question', getJourneyResponse, getJourney, validate, validationErrorHandler, saveToSession);
 
 	router.get('/check-your-answers', getJourneyResponse, getJourney, buildList());
-	router.post('/check-your-answers', getJourneyResponse, getJourney, buildSaveController(service));
+	router.post('/check-your-answers', getJourneyResponse, getJourney, saveToDatabase);
 
 	return router;
 }

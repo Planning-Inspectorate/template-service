b2d4d2b4443a38c49ae2a2ccee9fa894490e32e3
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-03-12 10:38:08 +0000

    chore: add eslint plugin to enforce file and folder naming

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import checkFile from 'eslint-plugin-check-file';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
@@ -19,6 +20,30 @@ export default defineConfig([
 		rules: {
 			'@typescript-eslint/consistent-type-imports': 'error',
 			'@typescript-eslint/no-explicit-any': 'off'
+		}
+	},
+	{
+		files: ['**/*.{js,ts}'],
+		ignores: ['tmp/**/*.*', 'node_modules/**/*.*'],
+		plugins: {
+			'check-file': checkFile
+		},
+		rules: {
+			'check-file/filename-naming-convention': [
+				'error',
+				{
+					'**/*.{js,ts}': 'KEBAB_CASE'
+				},
+				{
+					ignoreMiddleExtensions: true
+				}
+			],
+			'check-file/folder-naming-convention': [
+				'error',
+				{
+					'**/*': 'KEBAB_CASE'
+				}
+			]
 		}
 	}
 ]);

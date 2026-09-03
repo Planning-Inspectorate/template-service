2ecc19c8fc789b336b18dac988935dd860779c2d
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-09-03 16:20:50 +0100

    feat: add no-explicit-any rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,16 @@
 import { eslintConfig } from '@planning-inspectorate/coding-standards';
 import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
-export default defineConfig(eslintConfig);
+export default defineConfig([
+	...eslintConfig,
+	// TODO deprecate override when no-explicit-any is enabled in our coding-standards config
+	{
+		files: ['**/*.ts'],
+		extends: [tseslint.configs.recommended],
+		rules: {
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'@typescript-eslint/no-explicit-any': 'error'
+		}
+	}
+]);
